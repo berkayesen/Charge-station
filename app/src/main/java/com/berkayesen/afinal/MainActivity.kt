9997bf25c6d0b3aec5959b507ebcfca5325d2b09
BERKAY <BERKAY@DESKTOP-7K5KR5U>
2023-04-29 11:01:21 +0300

    commit

--- app/src/main/java/com/berkayesen/afinal/MainActivity.kt
+++ app/src/main/java/com/berkayesen/afinal/MainActivity.kt
@@ -1,13 +1,27 @@
 package com.berkayesen.afinal
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var  buttonLogin: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initViews()
 
         supportActionBar?.title = "Your Student ID"
+
+        buttonLogin.setOnClickListener{
+            val intent = Intent(this,SearchScreenActivity::class.java)
+            startActivity(intent)
+        }
     }
+    private fun initViews(){
+        buttonLogin = findViewById(R.id.buttonLogin)
+    }
+
 }
