6dd36727d1f2a888c25d45bafb868666e1689aba
BERKAY <BERKAY@DESKTOP-7K5KR5U>
2023-05-17 10:13:26 +0300

    creating MVVM

--- app/src/main/java/com/berkayesen/afinal/MainActivity.kt
+++ app/src/main/java/com/berkayesen/afinal/MainActivity.kt
@@ -4,10 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.activity.viewModels
+import com.berkayesen.afinal.viewmodel.MainActivityViewModel
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var  buttonLogin: Button
+    private val viewModel: MainActivityViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
