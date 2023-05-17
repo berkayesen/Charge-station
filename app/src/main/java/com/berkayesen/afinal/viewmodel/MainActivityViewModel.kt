a6f6f996987859377a44dca0a4d5d9e87e9c60c6
BERKAY <BERKAY@DESKTOP-7K5KR5U>
2023-05-17 10:13:36 +0300

    creating MVVM

---
+++ app/src/main/java/com/berkayesen/afinal/viewmodel/MainActivityViewModel.kt
@@ -0,0 +1,12 @@
+package com.berkayesen.afinal.viewmodel
+
+import android.content.Intent
+import androidx.core.content.ContextCompat.startActivity
+import androidx.lifecycle.ViewModel
+import com.berkayesen.afinal.SearchScreenActivity
+
+class MainActivityViewModel: ViewModel() {
+
+
+    }
+}
