34c8301ce7b797cdd73211b9ce0fef5f0f621a1b
BERKAY <BERKAY@DESKTOP-7K5KR5U>
2023-05-24 09:33:16 +0300

    requesting location permission

--- app/src/main/java/com/berkayesen/afinal/viewmodel/MapFragment.kt
+++ app/src/main/java/com/berkayesen/afinal/viewmodel/MapFragment.kt
@@ -1,10 +1,15 @@
 package com.berkayesen.afinal.viewmodel
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import com.berkayesen.afinal.databinding.FragmentMapBinding
 import com.huawei.hms.maps.HuaweiMap
 import com.huawei.hms.maps.MapView
@@ -16,6 +21,17 @@ class MapFragment : Fragment(), OnMapReadyCallback {
     private lateinit var binding: FragmentMapBinding
     private var mMapView: MapView? = null
     private var hMap: HuaweiMap? = null
+    companion object{
+        private const val TAG ="MapFragment"
+    }
+    val  requestMultiplePermission = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ){
+        permissions ->
+        permissions.entries.forEach{
+            Log.d(TAG,"${it.key} = ${it.value}")
+        }
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -32,6 +48,24 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         return binding.root
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestLocationPermission()
+    }
+    fun requestLocationPermission(){
+        if (!hasLocationPermission()){
+            requestMultiplePermission.launch(arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ))
+        }
+
+    }
+
+    private fun hasLocationPermission(): Boolean{
+        return ActivityCompat.checkSelfPermission(requireContext(),Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                || ActivityCompat.checkSelfPermission(requireContext(),Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+    }
     override fun onMapReady(p0: HuaweiMap?) {
         hMap = p0
     }
